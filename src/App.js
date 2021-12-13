28a4fb9983a2381cd93365f8bdcb97de75e4a7fb
Maheen Komal <maheenkomal992@gmail.com>
2021-12-13 19:37:43 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const App = () => {
+  const state = useState();
+  const [counter, setstate] = useState(0);
+
+  const Add = () => {
+    setstate(counter + 1);
+  };
+  const Sub = () => {
+    setstate(counter - 1);
+  };
+  const Reset = () => {
+    setstate(0);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h2>{counter}</h2>
+        <div className="bind">
+          <button onClick={Add}>Add</button>
+          <button onClick={Sub}>Sub</button>
+          <button onClick={Reset}>Reset</button>
+          
+        </div>
+      </div>
+    </>
   );
-}
-
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Detail from './Detail';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// const dte = new Date().toLocaleDateString();
+// const tme = new Date().toLocaleTimeString();
+// const name = 'Maheen Komal';
+// const storepic =
+//   'https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1925&q=80';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
+    <Detail />
     <App />
-  </React.StrictMode>,
+    {/* <h1 contentEditable="true">{`The developer name is ${name}`}</h1>
+    <h1>today date is = {dte}</h1>
+    <h1>today time is = {tme}</h1>
+    <div className="bind">
+      <img className="imgData" src={storepic} alt="missing" />
+      <img className="imgData" src={storepic} alt="missing" />
+      <img className="imgData" src={storepic} alt="missing" />
+    </div> */}
+  </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
